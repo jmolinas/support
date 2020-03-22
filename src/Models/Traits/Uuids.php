2ea3ccf727c65e31fc48c2beb79e379a1c578de8
Joseph Molinas <joseph_molinas@msn.com>
2020-03-23 00:25:40 +0800

    UUid trait

---
+++ src/Models/Traits/Uuids.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Gp\Support\Models\Traits;
+
+use Webpatser\Uuid\Uuid;
+
+trait Uuids
+{
+    /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(
+            function ($model) {
+                $model->{$model->getKeyName()} = Uuid::generate()->string;
+            }
+        );
+    }
+}
